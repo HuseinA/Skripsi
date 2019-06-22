ae1d80a5413286725f7d955329cc95c454b0736d
HuseinA <huseinabdulbar@gmail.com>
2019-06-22 16:38:23 +0700

    update

--- Mains.py
+++ Mains.py
@@ -1,16 +1,20 @@
 import pickle
 
-z=pickle.load(open('databin/hasil301015.p','rb'))
-for x in z:
+z=pickle.load(open('databin/hasil1201015.p','rb'))
+"""for x in z:
     print(x)
 for x in z:
-    print(x[2])
-z=[x for x in z if any(y in x[2] for y in x[1])]
+    print(x[2])"""
+z=[x for x in z if x[2]==x[1]]
+"""print([x[2] for x in z].count('Rock'))
+print([x[2] for x in z].count('Hip Hop'))
+print([x[2] for x in z].count('Jazz'))
+print([x[2] for x in z].count('Pop'))
+print([x[2] for x in z].count('Electronic'))"""
 print(len(z))
 print(z)
 
-"""import pickle
-x=[30,60,90,120]
+"""x=[30,60,90,120]
 y=[10,20,30]
 k=[5,10,15]
 
@@ -19,5 +23,5 @@ for a in x:
     for b in y:
         for c in k:
             z=pickle.load(open('databin/hasil'+str(a)+str(b)+str(c)+'.p','rb'))
-            z=[x for x in z if any(y in x[2] for y in x[1])]
+            z=[x for x in z if x[2]==x[1]]
             print('duration:'+str(b)+'\toffset:'+str(a)+'\tK:'+str(c)+'\tHasil:'+str(len(z)))"""
