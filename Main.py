8dda6d9d47851bdb2893a1d6f2d96322a1ebf605
HuseinA <huseinabdulbar@gmail.com>
2019-06-22 13:59:43 +0700

    finishing

--- Main.py
+++ Main.py
@@ -3,9 +3,12 @@ from statistics import mode
 from functools import reduce as rd
 import operator
 
-offs=[30,60,90,120]
-dur=[10,20,30]
-K=[5,10,15]
+dur=10
+offs=30
+K=5
+data=numpy.array(pickle.load(open('databin/latih'+str(offs)+str(dur)+'.p','rb')))
+tes=numpy.array(pickle.load(open('databin/uji'+str(offs)+str(dur)+'.p','rb')))
+kelas=pickle.load(open('databin/songclass.p','rb'))
 
 def cros_corr(train,test):
     return sum([a*b for a,b in zip(train,test)])/numpy.sqrt(sum([a**2 for a in train]) * sum([a**2 for a in test]))
@@ -20,7 +23,7 @@ def minmax(train,test):
     test=[[form(b[a],a) for a in range(4)]+[b[4]] for b in test]
     return train,test
 
-def init(uji,data,K,kelas):
+def init(uji):
     a=[[cros_corr(x[i],uji[i]) for i in range(4)] for x in data]
     a=[[4-sum(x),y[4]] for x,y in zip(a,data)]
 
@@ -33,32 +36,16 @@ def init(uji,data,K,kelas):
             res[x[2]][0]+=1
             res[x[2]][1]+=x[0]
 
-    test=[[v[0],k] for k,v in res.items()]
-    test.sort(reverse=True)
-
-    #print([x[:2] for x in a[:K]])
-
     try:
-        return mode([x[2] for x in a[:K]])
+        return [mode([x[2] for x in a[:K]])]
     except:
-        return min([v[1],k] for k,v in res.items() if v[0]==max(v[0] for v in res.values()))[1]
+        return [min([v[1],k] for k,v in res.items() if v[0]==max(v[0] for v in res.values()))[1]]
 
-def Main(dur,offs,K):
-    data=numpy.array(pickle.load(open('databin/latih'+str(offs)+str(dur)+'.p','rb')))
-    tes=numpy.array((pickle.load(open('databin/uji'+str(offs)+str(dur)+'.p','rb'))))
-    kelas=(pickle.load(open('databin/songclass.p','rb')))
-    data,tes=minmax(data,tes)
-
+def Main():
     z=[tes[i]+[kelas[x[4]]] for i,x in enumerate(tes) if x[4] in kelas]
-    z=[(x[4:]+[init(x,data,K,kelas)]) for x in z]
-    pickle.dump(z,open('databin/hasil'+str(offs)+str(dur)+str(K)+'.p','wb'))
+    z=[x[4:]+init(x) for x in z]
     z=[x for x in z if x[2] == x[1]]
     print(len(z))
-    print(z)
 
-for x in dur:
-    for y in offs:
-        for z in K:
-            print('duration:'+str(x)+'\toffset:'+str(y)+'\tK:'+str(z))
-            Main(x,y,z)
-            print('DONE') 
+data,tes=minmax(data,tes)
+Main()
