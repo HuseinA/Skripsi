213465eb3c06c8193b4640198d1ca95bce95290d
HuseinA <huseinabdulbar@gmail.com>
2019-09-11 17:18:23 +0700

    reformat with black

--- Feature_Extraction.py
+++ Feature_Extraction.py
@@ -3,7 +3,7 @@ from os import listdir
 from os.path import isfile, join
 
 data = "uji"
-dur = 10  # song duration
+dur = 5  # song duration
 offs = 120  # song offset
 directory = "../Music/" + data + "128kbps"
 
@@ -15,27 +15,15 @@ def extraction(song, offset, duration):
     frame_length = 512
     waveform, sr = librosa.load(song, offset=offset, duration=duration)
     zcr = librosa.feature.zero_crossing_rate(waveform)[0]  # zero crossing rate
-    spec_centroid = librosa.feature.spectral_centroid(waveform, sr=sr)[
-        0
-    ]  # spectral centroid
-    spec_rolloff = librosa.feature.spectral_rolloff(waveform + 0.01, sr=sr)[
-        0
-    ]  # spectral rolloff
-    energy = numpy.array(
-        [
-            sum(waveform[i : i + frame_length] ** 2)
-            for i in range(0, len(waveform), hop_length)
-        ]
-    )  # energy
+    spec_centroid = librosa.feature.spectral_centroid(waveform, sr=sr)[0]  # spectral centroid
+    spec_rolloff = librosa.feature.spectral_rolloff(waveform + 0.01, sr=sr)[0]  # spectral rolloff
+    energy = numpy.array([sum(waveform[i : i + frame_length] ** 2) for i in range(0, len(waveform), hop_length)])  # energy
     return [zcr, spec_centroid, spec_rolloff, energy, song[20:-4]]
 
 
 def run(dur, offs):
     print("databin/" + data + str(offs) + str(dur) + ".p")
-    result = [
-        extraction("../Music/" + data + "128kbps/" + waveform, offs, dur)
-        for waveform in file
-    ]
+    result = [extraction(directory + waveform, offs, dur) for waveform in file]
     print(result[0])
     print(len(result))
     pickle.dump(result, open("databin/" + data + str(offs) + str(dur) + ".p", "wb"))
@@ -46,8 +34,5 @@ def run(dur, offs):
 #    run(dur,waveform)
 
 # Use it to make single dataset
-result = [
-    extraction("../Music/" + data + "128kbps/" + waveform, offs, dur)
-    for waveform in file
-]
+result = [extraction(directory + waveform, offs, dur) for waveform in file]
 pickle.dump(result, open("databin/" + data + str(offs) + str(dur) + ".p", "wb"))
--- Main.py
+++ Main.py
@@ -7,21 +7,13 @@ dur = 10  # song duration
 offs = 120  # song offset
 K = 10  # K in KNN
 
-# dur=int(input('input durasi testing :'))
-# offs=int(input('input offset testing :'))
-# K=int(input('input K testing :'))
-
-train = numpy.array(
-    pickle.load(open("databin/latih" + str(offs) + str(dur) + ".p", "rb"))
-)
+train = numpy.array(pickle.load(open("databin/latih" + str(offs) + str(dur) + ".p", "rb")))
 test = numpy.array(pickle.load(open("databin/uji" + str(offs) + str(dur) + ".p", "rb")))
-kelas = pickle.load(open("databin/songclass.p", "rb"))
+clas = pickle.load(open("databin/songclass.p", "rb"))
 
 
 def cros_corr(train, test):
-    return sum([trn * tst for trn, tst in zip(train, test)]) / numpy.sqrt(
-        sum([trn ** 2 for trn in train]) * sum([trn ** 2 for trn in test])
-    )
+    return sum([trn * tst for trn, tst in zip(train, test)]) / numpy.sqrt(sum([trn ** 2 for trn in train]) * sum([trn ** 2 for trn in test]))
 
 
 def minmax(train, test):
@@ -39,18 +31,10 @@ def init(uji):
     distance = [[cros_corr(feature[i], uji[i]) for i in range(4)] for feature in train]
     distance = [[4 - sum(feature), genre[4]] for feature, genre in zip(distance, train)]
 
-    distance = [
-        distance[i] + [kelas[x[1]]] for i, x in enumerate(distance) if x[1] in kelas
-    ]
+    distance = [distance[i] + [clas[x[1]]] for i, x in enumerate(distance) if x[1] in clas]
     distance.sort(key=lambda x: x[0])
 
-    result = {
-        "Hip Hop": [0, 0],
-        "Pop": [0, 0],
-        "Electronic": [0, 0],
-        "Rock": [0, 0],
-        "Jazz": [0, 0],
-    }
+    result = {"Hip Hop": [0, 0], "Pop": [0, 0], "Electronic": [0, 0], "Rock": [0, 0], "Jazz": [0, 0]}
     for x in distance[:K]:
         if x[2] in result:
             result[x[2]][0] += 1
@@ -59,42 +43,36 @@ def init(uji):
     try:
         return [mode([x[2] for x in distance[:K]])]
     except:
-        return [
-            min(
-                [v[1], k]
-                for k, v in result.items()
-                if v[0] == max(v[0] for v in result.values())
-            )[1]
-        ]
+        return [min([v[1], k] for k, v in result.items() if v[0] == max(v[0] for v in result.values()))[1]]
 
 
-def Main():
-    index = int(input("Choose index song:"))
-    printHasil(test, index - 1)
-    # Pengujian()
+def main():
+    index = int(input("Choose index song: "))
+    print_result(test, index - 1)
+    # testing()
 
 
-def Pengujian():  # Confusion Matrix Multiclass Problem
+def testing():  # Confusion Matrix Multiclass Problem
     cmmp = [x[4:] + init(x) for x in test]
     # pickle.dump(cmmp,open('databin/hasil'+str(offs)+str(dur)+str(K)+'.p','wb'))
     cmmp = [x for x in cmmp if x[2] == x[1]]
     print(len(cmmp))
 
 
-def printHasil(cmmp, i):
+def print_result(cmmp, i):
     print("\nduration:" + str(dur) + "\toffset:" + str(offs) + "\tK:" + str(K))
     print("Song\t\t: " + cmmp[i][4])
     print("Actual\t\t: " + cmmp[i][5])
     print("Predicted\t: " + init(test[i])[0])
 
 
-def printLagu():
+def print_song():
     for i, x in enumerate(test):
         print("{} : {}".format(i + 1, x[4]))
 
-
-train, test = minmax(train, test)
-if input("Show Song List? (y/n) :") == "y":
-    printLagu()
-test = [test[i] + [kelas[x[4]]] for i, x in enumerate(test) if x[4] in kelas]
-Main()
+if __name__ == "__main__":
+    train, test = minmax(train, test)
+    if input("Show Song List? (y/n) : ") == "y":
+        print_song()
+    test = [test[i] + [clas[x[4]]] for i, x in enumerate(test) if x[4] in clas]
+    main()
